759b5dfa6a1446319daa9eecd6d7945210d137f9
raysinensis <raysinensis@gmail.com>
2021-06-02 22:15:09 -0600

    parse saf for psi value

--- inst/scripts/scraps_to_seurat.R
+++ inst/scripts/scraps_to_seurat.R
@@ -4,11 +4,18 @@
 #' @param object seurat object
 #' @param n_min  minimum number of observations for a site to be kept
 #' @param alt_only if TRUE, only keep genes with alternative polyA sites
+#' @param pf data.frame with SAF first column and position factor by gene, calculated by `parse_saf_pf`
+#' @return Seurat object with inserted assay
+#' @examples 
+#' s_small <- scraps_to_seurat("sample_R2_counts.tsv.gz",
+#'                             SeuratObject::pbmc_small,
+#'                             alt_only = FALSE)
 #' 
 scraps_to_seurat <- function(file, object, 
                              assay_name = "Asite",
                              n_min = 1,
-                             alt_only = TRUE) {
+                             alt_only = TRUE,
+                             pf = NULL) {
   # read file
   dat <- read_tsv(file)
   
@@ -38,9 +45,21 @@ scraps_to_seurat <- function(file, object,
     dat <- dat %>% filter(gene %in% keeps)
   }
   
+  # use psi values if pf is used
+  if (!is.null(pf)) {
+    dat <- dat %>% inner_join(pf, by = c("gene" = "GeneID")) %>% 
+      mutate(gene_name = str_remove(gene, "_.+")) %>% 
+      mutate(count2 = pf * count) %>% 
+      group_by(gene_name, cell) %>% 
+      summarize(count = sum(count2)/sum(count)) %>% 
+      ungroup() %>% 
+      rename(gene = "gene_name")
+  }
+  
   # transform to matrix
   mat <- dat %>% pivot_wider(names_from = cell, values_from = count, values_fill = 0) %>% 
     column_to_rownames("gene")
+  dat4 <<- mat
   
   # keep consistent cell ids
   emptys <- setdiff(Cells(object), colnames(mat))
@@ -57,6 +76,70 @@ scraps_to_seurat <- function(file, object,
   return(object)
 }
 
-s_small3_A <- scraps_to_seurat("sample_R2_counts.tsv.gz",
-                               clustifyr::s_small3,
-                               alt_only = FALSE)
+#' Parse SAF annotation file for position factor (pf) values
+#' 
+#' @param file SAF file used with Scraps
+#' @param alt_only if TRUE, only keep genes with alternative polyA sites
+#' @return data.frame with SAF first column and position factor by gene
+#' @examples 
+#' saf <- parse_saf_pf("ref/polyadb32.hg38.saf.gz")
+#' 
+parse_saf_pf <- function(file,
+                         alt_only = TRUE) {
+  # read SAF and split for gene symbol
+  saf <- read_tsv(file) %>% 
+    mutate(GeneID2 = str_remove(GeneID, Chr)) %>% 
+    separate(GeneID2, 
+             sep = "_", 
+             into = c("gene", 
+                      "genbank", 
+                      "id", 
+                      "chrom", 
+                      "pos",
+                      "strand",
+                      "class"),
+             remove = FALSE) %>% 
+    mutate(pos = as.numeric(pos)) %>% 
+    filter(gene != "NA") %>% 
+    filter(!is.na(pos)) %>% 
+    mutate(Start = Start - 1)
+  
+  # convert to bed format
+  bed1 <- saf %>% select(chrom = Chr, start = Start, end = End, strand, gene, GeneID)
+  
+  # remove symbols that come from different chromosomes
+  diffchrom <- bed1 %>% distinct(gene, chrom) %>% 
+    group_by(gene) %>% 
+    summarise(n = n()) %>% 
+    filter(n > 1) %>%
+    pull(gene)
+  
+  # position factor is 0-1
+  bed2_plus <- bed1 %>% 
+    filter(strand == "+") %>% 
+    filter(!(gene %in% diffchrom)) %>% 
+    group_by(gene) %>% 
+    arrange(.by_group = TRUE, start) %>% 
+    mutate(pf = row_number()) %>% 
+    mutate(pf = (pf - 1) / max(pf - 1)) %>% 
+    ungroup()
+  
+  bed2_minus <- bed1 %>% 
+    filter(strand == "-") %>% 
+    filter(!(gene %in% diffchrom)) %>% 
+    group_by(gene) %>% 
+    arrange(.by_group = TRUE, desc(start)) %>% 
+    mutate(pf = row_number()) %>% 
+    mutate(pf = (pf - 1) / max(pf - 1)) %>% 
+    ungroup()
+  
+  bed3 <- bind_rows(bed2_plus, bed2_minus)
+  
+  if (alt_only) {
+    bed3 <- bed3 %>% group_by(gene) %>% 
+      filter(n() > 1) %>% 
+      ungroup()
+  }
+  
+  bed3 %>% select(GeneID, pf)
+}
